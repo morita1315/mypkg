187ec60738e5cdbef1f11dca001814953f4482c6
Atsuya Morita <amazingpower1315@gmail.com>
2023-12-27 22:16:46 +0900

    Add a test

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,30 +1,16 @@
+#SPDX-FileCopyrightText: 2023 Atsuya Morita
+#SPDX-License-Indentifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
-from person_msgs.srv import Query
+from std_msgs.msg import Int16
 
-def main():
-    rclpy.init()
-    node = Node("listener")
-    client = node.create_client(Query, 'query')
-    while not client.wait_for_service(timeout_sec=1.0):
-        node.get_logger().info('待機中')
-    req = Query.Request()
-    req.name = "上田隆一"
-    future = client.call_async(req)
+def cb(msg):
+    global node
+    node.get_logger().info("Listen: %d" % msg.data)
 
-    while rclpy.ok():
-        rclpy.spin_once(node)
-        if future.done():
-            try:
-                response = future.result()
-            except:
-                node.get_logger().info('呼び出し失敗')
-            else:
-                node.get_logger().info("age: {}".format(response.age))
+rclpy.init()
+node = Node("listener")
+pub = node.create_subscription(Int16, "countup", cb, 10)
+rclpy.spin(node)
 
-            break
-    node.destroy_node()
-    rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,16 +1,20 @@
+#SPDX-FileCopyrightText: 2023 Atsuya Morita
+#SPDX-License-Indentifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
-from person_msgs.srv import Query 
+from std_msgs.msg import Int16
 
-def cb(request, response):
-    if request.name == "森田敦也":
-        response.age = 20
-    else:
-        response.age = 255
-
-    return response
- 
 rclpy.init()
 node = Node("talker")
-srv = node.create_service(Query, "query", cb)
-rclpy.spin(node)
+pub = node.create_publisher(Int16, "countup", 10) 
+n = 0
+
+ def cb():
+     global n
+     msg = Int16()
+     msg.data = n
+     pub.publish(msg)
+     n += 1
+node.create_timer(0.5, cb)
+rclpy.spin(node) 
